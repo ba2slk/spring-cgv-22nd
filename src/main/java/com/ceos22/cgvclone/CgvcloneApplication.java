a28796457e8402baa2f0307932957381061b78c9
ba2slk <0730bss@gmail.com>
2025-09-16 01:37:18 +0900

    add(spring-boot): project 초기 설정

---
+++ src/main/java/com/ceos22/cgvclone/CgvcloneApplication.java
@@ -0,0 +1,13 @@
+package com.ceos22.cgvclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgvcloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CgvcloneApplication.class, args);
+	}
+
+}
