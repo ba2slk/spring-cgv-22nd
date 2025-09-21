bb16b30c2a2e8dec4dec2492c967589921901978
ba2slk <0730bss@gmail.com>
2025-09-21 20:56:12 +0900

    add(feat): 영화 도메인 추가

---
+++ src/main/java/com/ceos22/cgvclone/domain/movie/Genre.java
@@ -0,0 +1,20 @@
+package com.ceos22.cgvclone.domain.movie;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Genre {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String type; // TODO: Genre 관리 전략
+}
---
+++ src/main/java/com/ceos22/cgvclone/domain/movie/Movie.java
@@ -0,0 +1,36 @@
+package com.ceos22.cgvclone.domain.movie;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private int duration;
+
+    private LocalDate releaseDate;
+    private LocalDate closeDate;
+
+    private String director;
+
+    @Builder.Default
+    private Long totalView = 0L;
+
+    @Builder.Default
+    private String rating = "ALL";
+
+    @Builder.Default
+    private Double star = 0.0;
+}
