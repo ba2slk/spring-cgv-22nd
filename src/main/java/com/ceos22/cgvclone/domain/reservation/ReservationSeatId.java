daa4fcc8511321838e951971164bb472daffc8bc
ba2slk <0730bss@gmail.com>
2025-09-21 20:56:37 +0900

    add(feat): 예약 도메인 추가

---
+++ src/main/java/com/ceos22/cgvclone/domain/reservation/ReservationSeatId.java
@@ -0,0 +1,13 @@
+package com.ceos22.cgvclone.domain.reservation;
+
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@NoArgsConstructor
+@EqualsAndHashCode
+public class ReservationSeatId implements Serializable {
+    private Long reservation;
+    private Long seat;
+}
