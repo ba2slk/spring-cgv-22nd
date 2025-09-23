7e2d692be05c6ae68616631db24b1b2427f2bcb3
ba2slk <0730bss@gmail.com>
2025-09-23 22:17:43 +0900

    add(feat): 영화 찜 엔터티 추가

---
+++ src/main/java/com/ceos22/cgvclone/domain/movie/MovieLikeRepository.java
@@ -0,0 +1,6 @@
+package com.ceos22.cgvclone.domain.movie;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MovieLikeRepository extends JpaRepository<MovieLike, Long> {
+}
