a1ec1edb3cf4378c6e0343b6803aebe4fba2d349
ba2slk <0730bss@gmail.com>
2025-09-24 16:22:32 +0900

    docs: Swagger 연동

--- src/main/java/com/ceos22/cgvclone/domain/snack/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ceos22.cgvclone.domain.snack.repository;
-
-import com.ceos22.cgvclone.domain.snack.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
